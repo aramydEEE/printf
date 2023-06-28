3cb7beba219e4caa4b61e764de0f3777e8e0ff14
Kelechi Okorie <kelechiokorie22@gmail.com>
2023-06-28 09:10:59 +0000

    Hot fix on digit_print and count_numbers

--- test/task1.c
+++ test/task1.c
@@ -11,14 +11,10 @@ int main(void)
 {
     int len;
     int len2;
-    unsigned int ui;
-    void *addr;
 
-	len2 = 500;
-	 len1 = 200;
+    len = 200;
+    len2 = 300;
 
-    ui = (unsigned int)INT_MAX + 1024;
-    addr = (void *)0x7ffe637541f0;
     _printf("Length:[%d, %i]\n", len, len);
     printf("Length:[%d, %i]\n", len2, len2);
     _printf("Negative:[%d]\n", -762534);
